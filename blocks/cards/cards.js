b24cdb70a8795f1ed63978daab222504efc9244c
ajaygouda <ajaygouda10@gmail.com>
2026-03-20 12:10:10 +0530

    Updating Card Comp

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,8 +4,8 @@
 export default function decorate(block) {
   const cardItems = [...block.children];
   block.innerHTML = '';
-  const cardData = cardItems.map((cardItem) => {
-    const cols = [cardItem.children][0];
+  const cardData = cardItems?.map((cardItem) => {
+    const cols = [cardItem?.children][0];
     return {
       banner: cols[0]?.querySelector('picture'),
       title: cols[1]?.querySelector('p')?.textContent,
@@ -16,14 +16,14 @@ export default function decorate(block) {
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = `
-        <div class="card__banner">${item?.banner.outerHTML}</div>
+        <div class="card__banner">${item?.banner?.outerHTML}</div>
         <div class="card__body">
           <h1 class="card__title">${item?.title}</h1>
           <button class="card__btn">View Details</buttton>
         </div>
     `
     block.appendChild(card);
-  })
+  });
   // ul.querySelectorAll('picture > img').forEach((img) => {
   //   const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
   //   moveInstrumentation(img, optimizedPic.querySelector('img'));
