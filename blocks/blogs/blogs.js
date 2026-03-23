e4bfa46328a5427ecb213b34a27de5de1436eac8
ajaygouda <ajaygouda10@gmail.com>
2026-03-23 23:09:53 +0530

    Adding

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -4,7 +4,7 @@ export default function decorate(block) {
   const blogsList = [...block.children];
   console.log(blogsList);
 
-  const blogData = blogsList.map((blog) => {
+  const blogsData = blogsList.map((blog) => {
     const cols = [...blog.children];
     console.log(cols[0]);
     return {
@@ -17,5 +17,35 @@ export default function decorate(block) {
       tags: cols[6]?.querySelector('p')?.textContent ?? '',
     };
   });
-  console.log('blogData', blogData);
+
+  blogsData.forEach((item) => {
+    const blog = document.createElement('div');
+    blog.classList.add('blog');
+    blog.innerHTML = `
+      <div class="blog__banner">${item?.banner}</div>
+      <div class="blog__body">
+        <p class="blog__category">${item?.category}</p>
+        <h1 class="blog__title">${item?.title}</h1>
+        <p class="blog__author-date">
+          <span>${item?.author}</span> | <span>${item?.postedDate}</span>
+        </p>
+      </div>
+    `;
+
+    blog.addEventListener('click', () => {
+      sessionStorage.removeItem('blogDetail');
+      sessionStorage.setItem('blogDetail', JSON.stringify({
+        banner: item?.banner,
+        title: item?.title,
+        description: item?.description,
+        postedDate: item?.postedDate,
+        author: item?.author,
+        tags: item?.category,
+      }));
+      const slug = item?.title?.toLowerCase().replace(/\s+/g, '-');
+      window.location.href = `/blogs/blog-detail?title=${slug}`;
+    });
+
+    block.appendChild(blog);
+  });
 }
