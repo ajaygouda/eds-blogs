7b4394529c63d69863dfc1527475ed1d1c6ef845
ajaygouda <ajaygouda10@gmail.com>
2026-03-24 12:17:38 +0530

    updating blog

--- blocks/blogs/blogs.js
+++ blocks/blogs/blogs.js
@@ -1,42 +1,23 @@
-import formatDate from '../../scripts/utils.js';
-
 export default function decorate(block) {
   const blogsList = [...block.children];
-  block.innerHTML = '';
 
   blogsList.forEach((blog) => {
     const cols = [...blog.children];
-    const item = {
-      banner: cols[0]?.querySelector('picture')?.outerHTML ?? '',
-      title: cols[1]?.querySelector('p')?.textContent ?? '',
-      description: cols[2]?.querySelector('p')?.textContent ?? '',
-      postedDate: formatDate(cols[3]?.querySelector('p')?.textContent),
-      author: cols[4]?.querySelector('p')?.textContent ?? '',
-      category: cols[5]?.querySelector('p')?.textContent ?? '',
-      tags: cols[6]?.querySelector('p')?.textContent ?? '',
-    };
 
-    const blogCard = document.createElement('div');
-    blogCard.classList.add('blog');
-    blogCard.innerHTML = `
-      <div class="blog__banner">${item.banner}</div>
-      <div class="blog__body">
-        <p class="blog__category">${item.category}</p>
-        <h1 class="blog__title">${item.title}</h1>
-        <p class="blog__author-date">
-          <span>${item.author}</span> | <span>${item.postedDate}</span>
-        </p>
-        <p class="blog__description">${item.description}</p>
-        <p class="blog__tags">${item.tags}</p>
-      </div>
-    `;
+    blog.classList.add('blog');
 
-    blogCard.addEventListener('click', () => {
-      sessionStorage.setItem('blogDetail', JSON.stringify(item));
-      const slug = item.title.toLowerCase().replace(/\s+/g, '-');
+    cols[0]?.classList.add('blog__banner');
+    cols[1]?.classList.add('blog__title');
+    cols[2]?.classList.add('blog__description');
+    cols[3]?.classList.add('blog__date');
+    cols[4]?.classList.add('blog__author');
+    cols[5]?.classList.add('blog__category');
+
+    blog.addEventListener('click', () => {
+      const title = cols[1]?.textContent;
+      const slug = title?.toLowerCase().replace(/\s+/g, '-');
+
       window.location.href = `/blogs/blog-detail?title=${slug}`;
     });
-
-    block.appendChild(blogCard);
   });
 }
